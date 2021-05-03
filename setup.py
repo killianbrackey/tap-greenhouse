47b5bc4d63a2fcdaa4411a449fa8d2e5c52cb73e
Killian Brackey <killian.brackey@sezzle.com>
2021-05-03 09:49:29 -0500

    Bumped singer python version to meet minimum requirements for target-postgres

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 from setuptools import setup
 setup(
     name="tap-greenhouse",
-    version="0.1.0",
+    version="0.1.1",
     description="Singer.io tap for extracting data",
     author="Simon Data",
     url="http://simondata.com",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_greenhouse"],
     install_requires=[
-        "singer-python==5.2.0",
+        "singer-python==5.9.0",
         'requests==2.18.4',
         "pendulum==1.2.0",
         "tap-kit @ git+https://github.com/Radico/tap-kit.git@master"
