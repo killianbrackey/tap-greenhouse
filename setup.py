d35e56f33a769586c96c108811ec3db7f47a837c
Killian Brackey <killian.brackey@sezzle.com>
2021-05-03 09:53:55 -0500

    Update to tap-kit with compatible python version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
         "singer-python==5.9.0",
         'requests==2.18.4',
         "pendulum==1.2.0",
-        "tap-kit @ git+https://github.com/Radico/tap-kit.git@master"
+        "tap-kit @ git+https://github.com/killianbrackey/tap-kit@master"
     ],
     dependency_links=[
         "https://github.com/Radico/tap-kit/tarball/master#egg=tap-kit-0.1.1",
